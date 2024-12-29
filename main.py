99242490e7c0acd668bf9c13dc6b603fbf7ad675
Ashwini <kakdeashwini12@gmail.com>
2024-12-29 12:45:44 +0530

    base model prepare is done

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion_pipeline import DataIngestionTrainningPipeline
+from cnnClassifier.pipeline.state_02_prepare_base_model import PrepareBaseModelTrainningPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainningPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME="Prepare Base Model Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = PrepareBaseModelTrainningPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
+
+#Data ingestion stage 1
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -8,4 +10,16 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
-    
+
+#Prepare base model stage2
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+   
